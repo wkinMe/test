8dd87ad59fd30f3c8b0f5fb9c14f14daac8f8b0d
name <you@example.com>
2025-03-24 14:04:18 +0300

    feat: Selection pipes, lists, stretgths has been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { ChangeEvent, useState } from 'react'
+import data from "./data/data.json";
+import config from "./data/config.json";
 import './App.css'
 
+const useParameters = () => {
+  const sizes = config.filter(i => i.type == "size")
+  const strengths = config.filter(i => i.type == "frame");
+
+  const width = sizes.find(i => i.key == "width");
+  const length = sizes.find(i => i.key == "length");
+  
+  return {sizes: {width, length}, strengths};
+}
+
+const useData = () => {
+  const pipes = data.filter(i => i.type == "pipe");
+  const lists = data.filter(i => i.type == "list");
+
+  return {pipes, lists}
+}
+
 function App() {
-  const [width, setWidth] = useState(10);
-  const [length, setLength] = useState(10);
-  const [materialId, setMaterialId] = useState(0);
-  const [pipeId, setPipeId] = useState(0);
-  const [strength, setStrength] = useState(0);
+  const {sizes, strengths} = useParameters();
+  const {pipes, lists} = useData();
+
+  const [selectedListId, setSelectedListId] = useState(0);
+  const [selectedPipeId, setSelectedPipeId] = useState(0);
+  const [selectedStrengthId, setStrengthId] = useState(0);
+
+
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const type = e.target.dataset.type;
+    const value = e.target.value;
+
+    switch(type) {
+      case "pipe": {
+        const selectedPipe = pipes.find(i => i.name == value)!;
+        const selectePipeIndex = pipes.indexOf(selectedPipe);
+        setSelectedPipeId(selectePipeIndex);
+        break;
+      }
+      case "list": {
+        const selectedList = lists.find(i => i.name == value)!;
+        const selectedListId = lists.indexOf(selectedList);
+        setSelectedListId(selectedListId);
+        break;
+      }
+      case "strength": {
+        const selectedStrength = strengths.find(i => i.name == value)!;
+        const selectedListId = strengths.indexOf(selectedStrength);
+        setStrengthId(selectedListId);
+        break;
+      }
+    }
+  }
 
   return (
     <>
-      <section>
-
-      </section>
-      <section>
-
-      </section>
-      <input type="number" />
-      <input type="number" />
-      <section>
-        
-      </section>
+      <select onChange={handleSelect} data-type="pipe">
+        {pipes.map(i => <option value={i.name}>{i.name}</option>)}
+      </select>
+      <select onChange={handleSelect} data-type="list">
+        {lists.map(i => <option value={i.name}>{i.name}</option>)}
+      </select>
+      <input type="number" max={sizes.width?.max} min={sizes.width?.min}/>
+      <input type="number" max={sizes.length?.max} min={sizes.length?.min}/>
+      <select onChange={handleSelect} data-type="strength">
+        {strengths.map(i => <option value={i.name}>{i.name}</option>)}
+      </select>
     </>
   )
 }
